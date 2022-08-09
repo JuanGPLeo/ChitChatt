027e63f75f214580f404ed2153340a8170190aa8
Juan Gabriel Pérez Leo <juangabriel.perez@globant.com>
2022-08-09 22:49:18 +0200

    Colors added

--- app/src/main/java/com/jgpleo/chitchatt/ui/theme/Color.kt
+++ app/src/main/java/com/jgpleo/chitchatt/ui/theme/Color.kt
@@ -6,3 +6,17 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val PrimaryColor = Color(0xFF00BF6D)
+val PrimaryColorVariant = Color(0xFF98D730)
+
+val ScreenBackgroundLight = Color.White
+val ScreenBackgroundDark = Color(0xFF1D1D35)
+
+val ElementsBackgroundLight = Color(0xFFEAF8F5)
+val ElementsBackgroundDark = Color(0xFF4A4AD5)
+
+val GrayDark800 = Color(0xFF44444E)
+val GrayDark600 = Color(0xFF6E6E7E)
+val GrayDark400 = Color(0xFFB7B7BE)
+val GrayDark200 = Color(0xFFEDEDEF)
